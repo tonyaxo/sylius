9641c35aa03ea0ff8c2cdcc583e61e0061f4caca
Sergey Bogatyrev <sergey@bogatyrev.me>
2020-05-18 00:07:08 +0000

    Add product options list
    Signed-off-by: Sergey Bogatyrev <sergey@bogatyrev.me>

---
+++ src/View/Product/ProductView.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\View\Product;
+
+use Sylius\ShopApiPlugin\View\Product\ProductView as BaseView;
+
+class ProductView extends BaseView
+{
+    /** @var array */
+    public $options = [];
+}
